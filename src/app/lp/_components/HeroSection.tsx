cf3e5072c553b44e2509c1c5ca95ce4b1b43bfae
Asuma Yamada <yamadaasuma@gmail.com>
2024-04-15 13:17:54 +0900

    fix: text

--- src/app/lp/_components/HeroSection.tsx
+++ src/app/lp/_components/HeroSection.tsx
@@ -2,7 +2,7 @@ const HeroSection = () => {
   return (
     <section className='mx-auto max-w-2xl py-36 text-center'>
       <h1 className='text-5xl font-extrabold text-slate-900'>
-        Unleash the Power of <br className='md:hidden' />
+        Unleash the Power of <br />
         <span className='bg-gradient-to-r from-blue-400 to-blue-700 bg-clip-text text-transparent'>
           Flow Data
         </span>
